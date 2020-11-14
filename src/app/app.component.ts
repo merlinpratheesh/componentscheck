4546555b103332c7598ab5d0478b2c377a84087f
merlinpratheesh <merlinpratheesh@gmail.com>
2020-11-14 16:44:58 +0530

    InitialCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'autocomplete-simple-example',
+  templateUrl: 'autocomplete-simple-example.html',
+  styleUrls: ['autocomplete-simple-example.css'],
 })
-export class AppComponent {
-  title = 'componentscheck';
+export class AutocompleteSimpleExample {
+  options: string[] = ['One', 'Two', 'Three'];
+  myControl = new FormControl();
+
 }
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
